1194aa7fffc4caa0874a61c8b8403a1a25453175
Kyle <xikkzz@gmail.com>
2021-12-29 01:15:16 -0500

    created component to clean up app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import ExpenseItem from "./components/ExpenseItem";
 import Expenses from "./components/Expenses";
 
 function App() {
@@ -27,7 +26,7 @@ function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      <Expenses />
+      <Expenses expenses={expenses} />
     </div>
   );
 }
