9e74b75840a2fbe597c377a125d1101fe0421943
Kyle <xikkzz@gmail.com>
2022-03-28 01:42:02 -0400

    dynamic code

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -4,7 +4,7 @@ import Card from "./Card";
 import "./Expenses.css";
 import ExpenseFilter from "./ExpensesFilter";
 
-const Expenses = ({ expenses }) => {
+const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2020");
 
   const filterChangeHandler = (selectedYear) => {
@@ -18,26 +18,13 @@ const Expenses = ({ expenses }) => {
           onChangeFilter={filterChangeHandler}
           selected={filteredYear}
         />
-        <ExpenseItem
-          title={expenses[0].title}
-          amount={expenses[0].amount}
-          date={expenses[0].date}
-        />
-        <ExpenseItem
-          title={expenses[1].title}
-          amount={expenses[1].amount}
-          date={expenses[1].date}
-        />
-        <ExpenseItem
-          title={expenses[2].title}
-          amount={expenses[2].amount}
-          date={expenses[2].date}
-        />
-        <ExpenseItem
-          title={expenses[3].title}
-          amount={expenses[3].amount}
-          date={expenses[3].date}
-        />
+        {props.expenses.map((expenses) => (
+          <ExpenseItem
+            title={expenses.title}
+            amount={expenses.amount}
+            date={expenses.date}
+          />
+        ))}
       </Card>
     </div>
   );
